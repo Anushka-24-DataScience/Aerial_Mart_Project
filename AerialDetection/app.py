284e9a5795f8e292fe73d26ff08a267cd96efb2f
Anushka-24-DataScience <shilpirbs@gmail.com>
2024-07-26 22:08:24 +0530

    training pipeline added

---
+++ AerialDetection/app.py
@@ -0,0 +1,3 @@
+from AerialDetection.pipeline.training_pipeline import TrainPipeline
+obj=TrainPipeline()
+obj.run_pipeline()
--- AerialDetection/pipeline/training_pipeline.py
+++ AerialDetection/pipeline/training_pipeline.py
@@ -1,22 +1,27 @@
-import sys, os
+
+import sys
+import os
 from AerialDetection.logger import logging
 from AerialDetection.exception import AppException
 from AerialDetection.components.data_ingestion import DataIngestion
 from AerialDetection.components.data_validation import DataValidation
-#from AerialDetection.components.model_trainer import ModelTrainer
+from AerialDetection.components.model_trainer import ModelTrainer
 
 
-from AerialDetection.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
-                                    
+from AerialDetection.entity.config_entity import (DataIngestionConfig,
+                                                 DataValidationConfig,
+                                                 ModelTrainerConfig)
 
-from AerialDetection.entity.artifacts_entity import (DataIngestionArtifact,DataValidationArtifact)
+from AerialDetection.entity.artifacts_entity import (DataIngestionArtifact,
+                                                    DataValidationArtifact,
+                                                    ModelTrainerArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-       
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -38,11 +43,13 @@ class TrainPipeline:
             )
 
             return data_ingestion_artifact
-        
+
         except Exception as e:
             raise AppException(e, sys)
         
 
+
+    
     def start_data_validation(
         self, data_ingestion_artifact: DataIngestionArtifact
     ) -> DataValidationArtifact:
@@ -68,12 +75,39 @@ class TrainPipeline:
             raise AppException(e, sys) from e
 
 
-    
-    
-    def run_pipeline(self)->None:
-        try:
-            data_ingestion_artifact=self.start_data_ingestion()
-            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
 
+    
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+        
+        
+
+        
+
+    
+
+    def run_pipeline(self) -> None:
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            
+            else:
+                raise Exception("Your data is not in correct format")
+
+        
         except Exception as e:
             raise AppException(e, sys)
